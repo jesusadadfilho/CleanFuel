1958abf5a3d1b67d9556039fb73425565af81de6
Jesus <jesusadadfilho@gmail.com>
2019-02-08 14:04:03 -0200

    login, singin e splashscreen

---
+++ app/src/main/java/com/example/jesus/cleanfuel/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.jesus.cleanfuel;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void singin(View view) {
+        startActivity(new Intent(this, SingInActivity.class));
+    }
+}
